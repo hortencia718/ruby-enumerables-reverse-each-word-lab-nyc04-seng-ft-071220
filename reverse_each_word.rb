47450a4944c5594ea01e839d8f33ac42c35ef9d4
hortencia c <hortenciaecisneros@gmail.com>
2020-08-06 17:13:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,4 +7,5 @@ end
   
 # create a arg __ always read the readme to look for a wrod that relates to the lab. 
 #  b/c i wanted to solve in one line i choose to look up how you would reverse a sentance and a word , and how you would put it together again .. 
-#  i used split
+#  i used split Search Results
+split is a String class method in Ruby which is used to split the given string into an array of substrings based on a pattern specified
