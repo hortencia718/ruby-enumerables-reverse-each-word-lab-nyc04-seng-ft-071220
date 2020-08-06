a5ec7aa3749553b2c24d84b6460146e7268c1eae
hortencia c <hortenciaecisneros@gmail.com>
2020-08-06 17:00:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str_arg)
-  new_array
+  # new_array
   reverse_each_word.select do |str_arg|
   str_arg.reverse()
   end
   
-reverse_each_word(new_array)
+reverse_each_word()
